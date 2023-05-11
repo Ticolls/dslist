f2304cb4c20deaa1fbf39c1c8599a0503842de77
Thiago Seixas <ticolls@hotmail.com>
2023-05-10 21:54:16 -0300

    GET game by id

--- src/main/java/com/ticolls/dslist/services/GameService.java
+++ src/main/java/com/ticolls/dslist/services/GameService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ticolls.dslist.dto.GameDTO;
 import com.ticolls.dslist.dto.GameMinDTO;
 import com.ticolls.dslist.entities.Game;
 import com.ticolls.dslist.repositories.GameRepository;
@@ -14,6 +15,13 @@ public class GameService {
 
     @Autowired
     private GameRepository repository;
+
+    public GameDTO findById(Long id) {
+        Game result = repository.findById(id).get();
+        GameDTO dto = new GameDTO(result);
+
+        return dto;
+    }
 
     public List<GameMinDTO> findAll() {
         List<Game> data = repository.findAll();
